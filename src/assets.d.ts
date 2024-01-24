b8b16629ebb5182bcb5e6f1758f0f32b6a88a98b
Louis <louis204@gmail.com>
2024-01-25 01:06:05 +0800

    header setup

---
+++ src/assets.d.ts
@@ -0,0 +1,3 @@
+declare module "@/assets/*";
+// This tells TypeScript that any import that matches @/assets/* can be imported as a module.
+// and it won't complain about not finding the module or its type declarations.
---
+++ src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div @click="goTo('/')" class="btn-tag">Home</div>
+    <div class="btn-tag">Products</div>
+    <div @click="goTo('/')">
+      <img class="icon btn-tag" :src="icon" alt="" />
+    </div>
+    <div @click="goTo('AboutMe')" class="btn-tag">About me</div>
+    <div class="btn-tag">Contact us</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import icon from "../assets/viaLactea-icon.png";
+import routes from "~pages";
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const goTo = (path: string) => router.push(path);
+</script>
+
+<style scoped>
+.header {
+  color: black;
+  display: flex;
+  gap: 3rem;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0 0.5rem 0;
+}
+
+.icon {
+  height: 3rem;
+}
+
+.btn-tag {
+  cursor: pointer;
+}
+</style>
